d46d318e1652361955954affce473cc30fd1cbae
juan jose suarez florez <jjsuarezf@ufpso.edu.co>
2025-03-11 19:42:18 -0500

    ejercicios.java

---
+++ ejercicios.java
@@ -0,0 +1,11 @@
+public class banc {
+    public static void main(String[] args) {
+        int Mes = 4;
+        float Retiro = 200f;
+        float saldoIni= 1000f;
+        float saldoFi = saldoIni-(Mes*Retiro);
+        System.out.println("Su saldo final de dinero fue  " + saldoFi);
+         
+       }
+   
+}
