a5f5311fd8eb1ed58ebb627bfba84fba71240058
juan jose <jjsuarezf@ufpso.edu.co>
2025-03-29 07:47:43 -0500

    ejerciciopr1.java

---
+++ src/ejerciciopr1.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+
+        import java.util.Scanner;
+
+
+    public static void main(String[] args) {
+        // Crear un array de tamaño 5
+        double[] numeros = new double[5];
+        double suma = 0;
+        Scanner scanner = new Scanner(System.in);
+        
+
+        System.out.println("Por favor, ingresa 5 números:");
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Número " + (i + 1) + ": ");
+            numeros[i] = scanner.nextDouble();  
+        }
+        
+      
+        for (int i = 0; i < 5; i++) {
+            suma += numeros[i];
+        }
+        
+        // Calcular el promedio
+        double promedio = suma / 5;
+        
+        // Imprimir el resultado
+        System.out.println("El promedio de los números es: " + promedio);
+        
+        scanner.close();  
+    
+
+
+    }
+}
